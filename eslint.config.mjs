d87e432dc30c6a2dfecfb1824a00b31bcad3d4b8
Andreas Rath <andreas.rath@ondewo.com>
2026-07-05 22:24:17 +0200

    build: track ondewo-survey-api OND211-2418-add-keycloak-for-2-fa branch and regenerate

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,19 @@ const compat = new FlatCompat({
 
 export default [
 	{
-		ignores: ['**/api/', 'src/ondewo-survey-api', '**/ondewo-proto-compiler', '**/example']
+		ignores: [
+			'**/api/',
+			'src/ondewo-survey-api',
+			'**/ondewo-proto-compiler',
+			'**/example',
+			'**/*.mjs',
+			'npm/',
+			'dist/',
+			'.test-build/',
+			'.test-build-examples/',
+			'auth/*.js',
+			'auth/*.d.ts'
+		]
 	},
 	{
 		files: ['**/*.js'], // Target all JavaScript files
